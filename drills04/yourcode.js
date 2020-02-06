ad6835bd78e872f314986749f0bffc6ab4b39b85
anjaleena <anjaleenab@github.com>
2020-02-06 15:39:44 -0800

    completed the changeElements function

--- drills04/yourcode.js
+++ drills04/yourcode.js
@@ -1,17 +1,51 @@
 
 
-function changeElements(){
+function changeElements(className){
+  debugger;
+var elements=document.querySelectorAll(className);
 
+for(var element=0;element < elements.length; element++) {
+var text=elements[element].innerText;
+
+switch (text) {
+  case 'one':
+    elements[element].innerText = 1;
+    break;
+  case 'two':
+    elements[element].innerText = 2;
+    break;
+  case 'three':
+    elements[element].innerText = 3;
+    break;
+  case 'four':
+    elements[element].innerText = 4;
+    break;
+  case 'five':
+    elements[element].innerText = 5;
+    break;
+  case 'six':
+    elements[element].innerText = 6;
+    break;
+  case 'seven':
+    elements[element].innerText = 7;
+    break;
+  case 'eight':
+    elements[element].innerText = 8;
+    break;
+  case 'nine':
+    elements[element].innerText = 9;
+    break;
+}
+}
 }
 
-function appendTextToElement(){
+// function appendTextToElement(){
 
-}
+// }
 
-function addClass(){
-}
+// function addClass(){
+// }
 
-function removeElements(){
+// function removeElements(){
 
-}
-
+// }
