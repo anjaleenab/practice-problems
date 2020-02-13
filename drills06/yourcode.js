16cd577364b3684d8a08517e0efd81a873fffb21
anjaleena <anjaleenab@github.com>
2020-02-12 21:54:33 -0800

    completed the putPosInElement function

--- drills06/yourcode.js
+++ drills06/yourcode.js
@@ -22,6 +22,12 @@ function addAttributeToElement(element, attribute, value ){
 $(element).attr(attribute, value);
 }
 
-function putPosInElement(  ){
-
+function putPosInElement(element){
+var pos =$(element).position();
+var objPos = {
+        "x": pos["top"],
+        "y": pos["left"]
+};
+$(element).html(`x: ${pos["top"]}px<br>y: ${pos["left"]}px`);
+return objPos;
 }
