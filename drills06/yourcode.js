94e150791ddd3e6e0258fd8e4bf760dd80870416
anjaleena <anjaleenab@github.com>
2020-02-12 21:12:44 -0800

    completed the removeClassFromElement function

--- drills06/yourcode.js
+++ drills06/yourcode.js
@@ -1,23 +1,21 @@
 
 
-function removeClassFromElement(){
-	
+function removeClassFromElement(element, className){
+	$(element).removeClass(className);
 }
 
 function toggleClassOnElement(){
-	
+
 }
 
 function hideElements(  ){
-	
+
 }
 
 function addAttributeToElement( ){
-	
+
 }
 
 function putPosInElement(  ){
-	
+
 }
-
-
